e1e9edd0d50165f8501e04cd45b7d1dc2162f8e6
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-11-17 09:23:20 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Input from './components/Input.jsx';
+import Login from "./components/Login.jsx";
+
+
+
+var isLoggedIn = true;
+
+const currentTime = new Date().getHours();
+
+console.log(currentTime);
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container"> {
+          // isLoggedIn  ? <h1>Hello</h1> : <Login />
+          currentTime < 12 ? <h1>Klokka er mer enn 12</h1> : <h1>Klokka er mindre enn 12</h1>
+
+        }</div>
+
+        
+
+
+
     </div>
+
   );
 }
 
